7db60c2d84bc1411e5222a4ee8f7d50281ac9466
Mike O'Donnell <mikeod38@gmail.com>
2024-03-25 16:46:24 -0400

    added SD helper function

--- R/theme_black.R
+++ R/theme_black.R
@@ -34,16 +34,18 @@ theme_black = function(base_size = 12, base_family = "") {
       #panel.border = element_blank(),
       #panel.background = element_blank(),
       panel.background = element_rect(fill = "black", color  =  NA),
-      panel.border = element_rect(fill = NA, color = "white"),
+      #panel.border = element_rect(fill = NA, color = "white"),
+      panel.border = element_rect(colour = "black", fill = NA),
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
       panel.spacing = unit(0.5, "lines"),
       # Specify facetting options
-      strip.background = element_rect(fill = "grey30", color = "grey10"),
+      #strip.background = element_rect(fill = "grey30", color = "grey10"),
+      strip.background = element_blank(),
       strip.text.x = element_text(size = base_size*0.8, color = "white"),
       strip.text.y = element_text(size = base_size*0.8, color = "white", angle = -90),
       #Specify plot options
-      plot.background = element_rect(color = "black", fill = "black"),
+      plot.background = element_rect(color = "black", fill= "black", size =5),
       plot.title = element_text(size = base_size*1.2, color = "white")
       #plot.spacing = unit(rep(1, 4), "lines")
 
